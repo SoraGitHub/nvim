626634411f89e293539ca8b2aa9979686d8bea88
Sora <chatwithsora@gmail.com>
2022-06-15 22:18:34 +0800

    nvim config

---
+++ init.lua
@@ -0,0 +1,24 @@
+-- 基础配置
+require("basic")
+-- 快捷键映射
+require("keybindings")
+-- Packer 插件管理
+require("plugins")
+-- 主题设置
+require("colorscheme")
+-- 插件配置
+require("plugin-config.nvim-tree")
+require("plugin-config.lualine")
+require("plugin-config.telescope")
+require("plugin-config.dashboard")
+require("plugin-config.project")
+require("plugin-config.nvim-treesitter")
+require("plugin-config.indent-blankline")
+require("plugin-config.nvim-autopairs")
+require("plugin-config.comment")
+require("plugin-config.surround")
+
+-- 内置LSP
+require("lsp.setup")
+require("lsp.cmp")
+require("lsp.ui")
